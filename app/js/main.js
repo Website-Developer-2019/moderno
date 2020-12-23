81f4265d2c89ea3ddaec7d573972d107138d9735
Афанасьев Сергей Аверьянович <C:\Users\AfanasevSA\AppData\Roaming\The Bat!>
2020-12-23 13:24:02 +0700

    adaptive header and footer

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,5 @@
+const { active } = require("browser-sync");
+
 $(function() {
 
 
@@ -16,6 +18,40 @@ $(function() {
         slidesToScroll: 4
 
     });
+
+    $(".js-range-slider").ionRangeSlider({
+        type: "double",
+
+        min: 0,
+        max: 1000,
+        from: 0,
+        to: 600,
+        prefix: "$"
+    });
+
+    $('.icon-th-list').on('click', function() {
+        $('.products__item').addClass('list');
+        $('.icon-th-list').addClass('active');
+        $('.icon-th-large').removeClass('active');
+    });
+
+    $('.icon-th-large').on('click', function() {
+        $('.products__item').removeClass('list');
+        $('.icon-th-large').addClass('active');
+        $('.icon-th-list').removeClass('active');
+
+    });
+
+
+
+    $('.menu__btn').on('click', function() {
+        $('.menu__list').slideToggle();
+    });
+
+    $('.header__btn-menu').on('click', function() {
+        $('.header__box').toggleClass('active');
+    });
+
 
     var mixer = mixitup('.products__inner-box');
 
